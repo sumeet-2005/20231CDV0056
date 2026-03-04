30d4ab8df93d88135fe59deb0ecf7bb4c11aa4b4
Sumeet Biradar <sumeetbiradar444@gmail.com>
2026-03-04 05:48:46 +0000

    fixed java error

--- Evenodd.java
+++ Evenodd.java
@@ -3,12 +3,12 @@ class Evenodd{
         int num;
         if(num%2==0){
             System.out.println("Number is Even");
+        }
         else if(num<0){
             System.out.println("Invalid number");
         }
         else{
             System.out.println("Number is odd");
-        }
         }
         }
     }
