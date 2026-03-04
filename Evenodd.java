b0eca41c59cc072ce61bc06a2b679c3ee9dea6d8
Sumeet Biradar <sumeetbiradar444@gmail.com>
2026-03-04 05:47:20 +0000

    added github actions

---
+++ Evenodd.java
@@ -0,0 +1,14 @@
+class Evenodd{
+    public static void main(String[] args){
+        int num;
+        if(num%2==0){
+            System.out.println("Number is Even");
+        else if(num<0){
+            System.out.println("Invalid number");
+        }
+        else{
+            System.out.println("Number is odd");
+        }
+        }
+        }
+    }
