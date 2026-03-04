39e9acc7f5de4ff3dc91adee8d413fc261b679b6
Sumeet Biradar <sumeetbiradar444@gmail.com>
2026-03-04 05:49:51 +0000

    fixed

--- Evenodd.java
+++ Evenodd.java
@@ -1,6 +1,6 @@
 class Evenodd{
     public static void main(String[] args){
-        int num;
+        int num=10;
         if(num%2==0){
             System.out.println("Number is Even");
         }
